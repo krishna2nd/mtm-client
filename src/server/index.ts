9b7740c7c18f2157ab6c1c1e3d0eb24ccb5a9f8d
Krishna Kumar <krk@microsoft.com>
2019-11-21 10:31:13 +0530

    Updated with new site

--- src/server/index.ts
+++ src/server/index.ts
@@ -8,6 +8,6 @@ var serveStatic = require('serve-static')
 app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use(morgan(':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent"'))
-app.get('/', serveStatic('dist/public'));
+app.get('/*', serveStatic('dist/public'));
  
 app.listen(process.env.PORT || 9091)
